e22457194ed03727e128bb0438a68e1becb4e6ae
leesibin <tlqls123@gmail.com>
2023-01-27 17:48:15 +0900

    저장

--- tsf/src/views/AboutView.vue
+++ tsf/src/views/AboutView.vue
@@ -1,5 +1,34 @@
 <template>
-  <div class="about">
-    <h1>This is an about page111</h1>
-  </div>
+  <div>내페이지가 맞습니까?</div>
+  <img id="user_image" src="#" alt="" />
+  <input
+    accept=".png"
+    type="file"
+    id="user_profile_img"
+    on-change="PreviewImage()"
+  />
 </template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {};
+  },
+  methods: {
+    PreviewImage() {
+      var preview = new FileReader();
+      preview.onload = function (e) {
+        // img id 값
+        document.getElementById("user_image").src = e.target.result;
+      };
+      // input id 값
+      preview.readAsDataURL(
+        document.getElementById("user_profile_img").files[0]
+      );
+    },
+  },
+};
+</script>
+
+<style></style>

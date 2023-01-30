7abe6e37785dcc59a78ed4e7291039f2c2a6c76c
leesibin <tlqls123@gmail.com>
2023-01-30 15:51:38 +0900

    1/30저장

---
+++ dist.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const app = express();
+const port = 3000;
+const Photo = require("./ori.js");
+const _path = path.join(__dirname, "./dist");
+// console.log(_path);
+app.use("/", express.static(_path));
+app.use(logger("tiny"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/about", function (req, res) {
+  const A = req.body.imageUrl
+  const main = async () => {
+    const _data = {
+      이미지base64 : A,
+    };
+
+    const new_photo = new Photo(_data);
+    const t = await new_photo.save();
+    console.log(t);
+  };
+  main();
+});
+
+app.listen(port, () => {
+  console.log(port + "에서 서버 동작 완료.");
+});
--- tsf/src/views/AboutView.vue
+++ tsf/src/views/AboutView.vue
@@ -2,10 +2,11 @@
   <h2>이건 양형님에게 잘 보일 기회야</h2>
   <input @change="upload" type="file" id="file" class="inputfile"  accept ="image/* " />
   <br>
-    <img v-bind:src="imageUrl">
+    <img v-bind:src="images">
 </template>
 
 <script>
+import axios from "axios";
 export default{
   name:'app',
 data() {
@@ -17,10 +18,22 @@ data() {
     upload(e) {
 
 let imageFile = e.target.files; // 업로드한 파일의 데이터가
-console.log(imageFile[0]); // 업로드한 파일의 데이터가 확인
 let url = URL.createObjectURL(imageFile[0]); // 파일의 필요한 데이터만을 url 변수에 넣음
-console.log(url); // 확인
 this.imageUrl = url; // 미리 작성해둔 imageUrl : ' ' 변수에 가지고있는 경로데이터를 넣음
+this.imageUrl = window.btoa(url)
+console.log(imageFile[0]); // 업로드한 파일의 데이터가 확인
+console.log(url); // 확인
+
+
+axios({
+        url: "http://localhost:3000/about",
+        method: "POST", // 전송방식을 post로 지정
+        data: {
+          imageUrl: this.imageUrl,
+        },
+      }).then((res) => {
+        alert(res.data.message);
+      });
 
 },
   }
